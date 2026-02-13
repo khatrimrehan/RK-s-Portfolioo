fed110a84c7480ae01708766ccfcf76f80c164e1
RK <khatrimrehan@gmail.com>
2026-02-13 21:32:33 +0530

    Refactor mousemove events and enhance animations
    Added delays to mousemove event handlers for cursor and blur elements. Enhanced animations for navigation and main sections with GSAP.

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+
+
 var csr = document.querySelector("#cursor")
 var blr = document.querySelector("#blur")
+
 document.addEventListener("mousemove", function (dets) {
-    csr.style.left = dets.x + 1 +"px"
-    csr.style.top = dets.y - 2 + "px"
-    blr.style.left = dets.x - 150 +"px"
-    blr.style.top = dets.y - 150 + "px"
+
+    setTimeout(function () {
+
+        csr.style.left = dets.x + 0 + "px"
+        csr.style.top = dets.y - 3 + "px"
+
+
+
+    }, 130) // delay in milliseconds (100ms)
+
+})
+document.addEventListener("mousemove", function (dets) {
+
+    setTimeout(function () {
+        blr.style.left = dets.x - 150 + "px"
+        blr.style.top = dets.y - 150 + "px"
+
+
+
+    }, 90) // delay in milliseconds (100ms)
+
 })
 
 
@@ -13,10 +33,8 @@ document.addEventListener("mousemove", function (dets) {
 
 
 
-
-
 gsap.to("#nav", {
-    backgroundColor: "#000000b5",
+    backgroundColor: "#0b0b0bc7",
     backdropFilter: "blur(10px)",
     duration: 0.5,
     height: "65.27px",
@@ -37,5 +55,37 @@ gsap.to("#main", {
         start: "top -30%",
         end: "top -80%",
         scrub: 2
+    }
+})
+
+gsap.from(".lft,.rght", {
+    y: 80,
+    opacity: 0,
+    duration: 1,
+    delay: 0.3,
+    stagger:0.4,
+    scrollTrigger: {
+        trigger: "#page2",
+        scroller: "body",
+        start: "top 60%",
+        end:"top 0%",
+        
+    }
+})
+
+gsap.from(".pics", {
+    Z: 80,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.4,
+    
+    scrollTrigger: {
+        trigger: ".pics",
+        scroller: "body",
+        markers: true,
+        scrub: 2,
+        start:"top 70%",
+        end:"top 40%"
+        
     }
 })
